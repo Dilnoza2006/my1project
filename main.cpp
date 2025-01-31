9858f79256d34428a5b31fe329fe9db3e2c011f9
d.saitqulova <d.saitqulova@newuu.uz>
2025-01-31 10:53:11 +0500

    Main updated

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ int main() {
     // cout<<"you are "<<age<<"years old"<< endl;
     // return 0;
 
+    //problem2
     // int a=1 ,b=2 ,c=3, d=4 ,e=5;
     // cout<<setw(5)<<a<<endl;
     // cout<<setw(5)<<b<<endl;
@@ -37,7 +38,8 @@ int main() {
     // cout << "before swapping" <<endl;
     // cout << "first integer is " << a << endl
     // << "second integer is " << b << endl;
-    //
+
+    //problem4
     // int temp=a;
     // a=b;
     // b=temp;
@@ -46,13 +48,107 @@ int main() {
     // cout << "second integer is " << b << endl;
     // return 0;
 
-    string input;
-    cout << "Please enter a string" << endl;
-    cin >> input;
-    int width=50;
-    int padding=(width-input.length())/2;
-    cout<<setw(padding)<<input<<setw(padding)<<endl;
+    //problem5
+    // string input;
+    // cout << "Please enter a string" << endl;
+    // cin >> input;
+    // int width=50;
+    // int padding=(width-input.length())/2;
+    // cout<<setw(padding)<<input<<setw(padding)<<endl;
 
-    return 0;
+    //lab_3
+    //problem1
+    // int a, b, c, d,e ;
+    // cout<< "enter  5 integers"<< endl;
+    // cin >> a>> b>> c>>d >>e;
+    // int sum=a+b+c+d+e;
+    // cout<< "sum of your integer is equal to "<<sum<<endl;
+    // return 0;
+
+    //problem2
+    // int student_id,  year_of_study,study_fee;
+    // float gpa;
+    // cout<<"enter your student ID:"<<endl;
+    // cin>>student_id;
+    // cout<<"enter your GPA:"<<endl;
+    // cin>>gpa;
+    // cout<<"enter your Year of study:"<<endl;
+    // cin>>year_of_study;
+    // cout<<"enter your study_fee:"<<endl;
+    // cin>>study_fee;
+    // cout << "student ID: " << student_id << endl;
+    // cout << "GPA: " << gpa << endl;
+    // cout << "Year of study: " << year_of_study << endl;
+    // cout << "Study fee: " << study_fee<< "sum"<<endl;
+    // return 0;
+
+    //problem3
+    // int celcius,fahrenheit;
+    // cout<<"Enter the temperature in celcius: ";
+    // cin>>celcius;
+    // fahrenheit = (celcius * 1.8) + 32;
+    // cout<<"The temperature in Celcius: "<<celcius<<endl;
+    // cout<<"The temperature in Fahrenheit: "<<fahrenheit<<endl;
+    // return 0;
+
+    //problem4
+    // int  quantity1, quantity2;
+    // double price1,price2, total;
+    // cout << "Enter price of apple: ";
+    // cin >> price1;
+    // cout << "Enter quantity apple: ";
+    // cin >> quantity1;
+    // cout<<"Enter price of bread: ";
+    // cin >> price2;
+    // cout<<"Enter quantity bread: ";
+    // cin >> quantity2;
+    //
+    // total = price1 * quantity1+price2 * quantity2;
+    // cout<<"Total cost: "<<total;
+    // return 0;
+
+    //problem5
+    // float age_of_dad,age_of_mom,age_of_sibling,age_of_you;
+    // float avg;
+    // cout << "Enter your father's age: ";
+    // cin >> age_of_dad;
+    // cout << "Enter your mother's age: ";
+    // cin >> age_of_mom;
+    // cout << "Enter your sibling's age: ";
+    // cin >> age_of_sibling;
+    // cout << "Enter your you age: ";
+    // cin >> age_of_you;
+    // avg=(age_of_mom+age_of_sibling+age_of_you+age_of_dad)/4.0;
+    // cout<<"average age of a family is : "<<avg<<endl;
+    // return 0;
+
+    //problem6
+    // float width, length, area, perimeter;
+    // cout << "Enter the length of the rectangle: ";
+    // cin >> length;
+    // cout << "Enter the width of the rectangle: ";
+    // cin >> width;
+    // area = (length * width);
+    // perimeter = 2 *(length + width);
+    // cout<<"Area of the rectangle is: "<<area<<endl;
+    // cout<<"Perimeter of the rectangle is: "<<perimeter<<endl;
+    // return 0;
+
+    //problem7
+    // int devidend, divisor, quotient, remainder;
+    // cout << "Enter a devidend: ";
+    // cin >> devidend;
+    // cout << "Enter a divisor: ";
+    // cin >> divisor;
+    // quotient = devidend / divisor;
+    // remainder = devidend % divisor;
+    // cout << "The quotient of the division is : "<< quotient << endl;
+    // cout << "The remainder of the division is : " << remainder << endl;
+    // return 0;
+
+
+
+
+
 }
 
